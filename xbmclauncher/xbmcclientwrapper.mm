787a03bc63f1dc215eada6d528e9783d399c2d5c
stephan.diederich <stephan.diederich@af6a6984-82a5-11dd-ad27-0f66e57d2035>
2008-09-25 00:38:40 +0000

    replaced NSLogs with DebugHelpers so they go away in Release build

--- xbmclauncher/xbmcclientwrapper.mm
+++ xbmclauncher/xbmcclientwrapper.mm
@@ -6,9 +6,22 @@
  *  Copyright 2008 University Heidelberg. All rights reserved.
  *
  */
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "xbmcclientwrapper.h"
 #include "xbmcclient.h"
+#include "XBMCDebugHelpers.h"
 #include <iostream>
 #include <map>
 
@@ -33,7 +46,7 @@ typedef std::map<eATVClientEvent, CPacketBUTTON*> tEventMap;
 }
 
 -(id) initWithServerAdress:(NSString*) fp_adress{
-	NSLog(@"XBMCClientImpl initWithServerAdress");
+	PRINT_SIGNATURE();
 	if ( ![super init] )
 		return nil;
 		
@@ -52,7 +65,7 @@ typedef std::map<eATVClientEvent, CPacketBUTTON*> tEventMap;
 }
 
 -(void) dealloc{
-	NSLog(@"XBMCClientImpl dealloc");
+	PRINT_SIGNATURE();
 	[mp_adress retain];
 	delete mp_event_map;
 	[super dealloc];
@@ -93,7 +106,7 @@ typedef std::map<eATVClientEvent, CPacketBUTTON*> tEventMap;
 
 @implementation XBMCClientWrapper
 -(id) init{
-	NSLog(@"XBMCClient init...");
+	PRINT_SIGNATURE();
 	if( ![super init] )
 		return nil; 
 	mp_impl = [[XBMCClientWrapperImpl alloc] initWithServerAdress:@"localhost"];
@@ -101,7 +114,7 @@ typedef std::map<eATVClientEvent, CPacketBUTTON*> tEventMap;
 }
 
 - (void)dealloc{
-	NSLog(@"XBMCClient deallocating...");
+	PRINT_SIGNATURE();
 	[mp_impl release];
 	[super dealloc];
 }
