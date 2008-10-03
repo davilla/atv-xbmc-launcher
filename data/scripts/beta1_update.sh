d15296e9d371d5b22cb6a2209a9387b5cfa01d68
stephan.diederich <stephan.diederich@af6a6984-82a5-11dd-ad27-0f66e57d2035>
2008-10-03 11:56:31 +0000

    added mount r/w check; simplify sudo usage by restarting script if not run as root

--- data/scripts/beta1_update.sh
+++ data/scripts/beta1_update.sh
@@ -1,26 +1,40 @@
 #!/bin/sh
 
 DISKIMAGE=$1
+PW="frontrow"
+REMOUNT=0
 
 echo "Installing from diskimage $DISKIMAGE"
+
+# make sure we're running as root, if not relaunch us
+if [ "$USER" != "root" ]; then
+  echo $PW | sudo -S "$0" $*
+  exit 0
+fi
+
 # check that disk-image exists
 if [ -e $DISKIMAGE ]; then
- PW="frontrow"
 
- #TODO check and store read/write
-
+ # Check if / is mounted read only
+ if mount | grep ' on / '  | grep -q 'read-only'; then
+  REMOUNT=1
+  /sbin/mount -uw /
+ fi
+ 
  # install xbmc app
- echo $PW | sudo -S hdiutil attach $DISKIMAGE 
- echo $PW | sudo -S installer -pkg /Volumes/XBMC/"XBMC Media Center.mpkg" -target /
- echo $PW | sudo -S hdiutil detach /Volumes/XBMC
+ hdiutil attach $DISKIMAGE 
+ installer -pkg /Volumes/XBMC/"XBMC Media Center.mpkg" -target /
+ hdiutil detach /Volumes/XBMC
 
  # handle any post-install items here
 
- #TODO
- #restore read/write settings
+ # remount root as we found it
+ if [ "$REMOUNT" = "1" ]; then
+  /sbin/mount -ur /
+ fi
  
  # clean up
- # echo $PW | sudo -S rm -rf /Users/Frontrow/staging/xbmc
+
  exit 0
 fi
 echo "Failed to find diskimage $DISKIMAGE"
