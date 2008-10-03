f2ca7aa6b603df4e64829f1ee0a771d242666cb0
davilla@4pi.com <davilla@4pi.com@af6a6984-82a5-11dd-ad27-0f66e57d2035>
2008-10-03 16:20:31 +0000

    fix install script

--- data/scripts/beta1_update.sh
+++ data/scripts/beta1_update.sh
@@ -1,41 +1,62 @@
 #!/bin/sh
 
 DISKIMAGE=$1
-PW="frontrow"
-REMOUNT=0
 
 echo "Installing from diskimage $DISKIMAGE"
+# check that disk-image exists
+if [ -e $DISKIMAGE ]; then
+  PW="frontrow"
 
-# make sure we're running as root, if not relaunch us
-if [ "$USER" != "root" ]; then
-  echo $PW | sudo -S "$0" $*
+  # check and store OSBoot read/write settings
+  REMOUNT=0
+  # Check if / is mounted read only
+  if mount | grep ' on / '  | grep -q 'read-only'; then
+    REMOUNT=1
+    echo $PW | sudo -S /sbin/mount -uw /
+  fi
+
+  # install xbmc app
+  echo $PW | sudo -S hdiutil attach $DISKIMAGE 
+  echo $PW | sudo -S installer -pkg /Volumes/XBMC/"XBMC Media Center.mpkg" -target /
+  echo $PW | sudo -S hdiutil detach /Volumes/XBMC
+
+  # handle any post-install items here
+  # perl to bash convertion of postflight script pending
+  if [ ! -d /Users/frontrow/Movies ]; then
+    echo $PW | sudo -S mkdir /Users/frontrow/Movies
+    echo $PW | sudo -S chown frontrow:frontrow /Users/frontrow/Movies
+  fi
+  if [ ! -d "/Users/frontrow/Video Playlists" ]; then
+    echo $PW | sudo -S mkdir "/Users/frontrow/Video Playlists"
+    echo $PW | sudo -S chown "/Users/frontrow/Video Playlists"
+  fi
+  if [ ! -d /Users/frontrow/Music ]; then
+    echo $PW | sudo -S mkdir /Users/frontrow/Music
+    echo $PW | sudo -S chown frontrow:frontrow /Users/frontrow/Music
+  fi
+  if [ ! -d "/Users/frontrow/Music Playlists" ]; then
+    echo $PW | sudo -S mkdir "/Users/frontrow/Music Playlists"
+    echo $PW | sudo -S chown "/Users/frontrow/Music Playlists"
+  fi
+  if [ ! -d /Users/frontrow/Pictures ]; then
+    echo $PW | sudo -S mkdir /Users/frontrow/Pictures
+    echo $PW | sudo -S chown frontrow:frontrow /Users/frontrow/Pictures
+  fi
+  
+  # restore OSBoot read/write settings
+  if [ "$REMOUNT" = "1" ]; then
+    echo $PW | sudo -S /sbin/mount -ur /
+  fi
+
+  # clean up
+  if [ -e /Users/frontrow/Movies/XBMC ]; then
+  # something makes this symlink during install so zap it.
+  echo $PW | sudo -S rm /Users/frontrow/Movies/XBMC
+  fi
+
+
   exit 0
 fi
 
-# check that disk-image exists
-if [ -e $DISKIMAGE ]; then
-
- # Check if / is mounted read only
- if mount | grep ' on / '  | grep -q 'read-only'; then
-  REMOUNT=1
-  /sbin/mount -uw /
- fi
- 
- # install xbmc app
- hdiutil attach $DISKIMAGE 
- installer -pkg /Volumes/XBMC/"XBMC Media Center.mpkg" -target /
- hdiutil detach /Volumes/XBMC
-
- # handle any post-install items here
-
- # remount root as we found it
- if [ "$REMOUNT" = "1" ]; then
-  /sbin/mount -ur /
- fi
- 
- # clean up
-
- exit 0
-fi
 echo "Failed to find diskimage $DISKIMAGE"
 exit -1
