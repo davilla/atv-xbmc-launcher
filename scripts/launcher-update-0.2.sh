4fbff39edaf171eb8f81f4e77123c246db1513dd
stephan.diederich <stephan.diederich@af6a6984-82a5-11dd-ad27-0f66e57d2035>
2008-10-04 19:16:14 +0000

    remove installer and restart finder by ourselved, as xbmclauncher gets killed and can't do it

--- scripts/launcher-update-0.2.sh
+++ scripts/launcher-update-0.2.sh
@@ -17,16 +17,22 @@ if [ -e $INSTALLER ]; then
 
  #  install new launcher
  echo $PW | sudo -S chmod +x $INSTALLER
- echo $PW | sudo -S $INSTALLER -- installrestart
+ echo $PW | sudo -S $INSTALLER -- install /
 
  #sync to disk, just in case...
  /bin/sync
+
+ #remove the download
+ rm $INSTALLER 
 
  # remount root as we found it
  if [ "$REMOUNT" = "1" ]; then
   echo $PW | sudo -S /sbin/mount -ur /
  fi
 
+ # restart finder
+ kill `ps awx | grep [F]inder | awk '{print $1}'`
+ 
  exit 0
 fi
 echo "Failed to find installer $INSTALLER"
