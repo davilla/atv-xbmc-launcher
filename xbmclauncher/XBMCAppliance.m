65c4bfc1e3d336425044aff2a921e92ea1cbaf82
stephan.diederich <stephan.diederich@af6a6984-82a5-11dd-ad27-0f66e57d2035>
2008-10-17 17:04:29 +0000

    try to put whole license on screen

--- xbmclauncher/XBMCAppliance.m
+++ xbmclauncher/XBMCAppliance.m
@@ -105,18 +105,17 @@ typedef enum {
     version = [NSNumber numberWithInt:-1];
   else
     version = [info_dic objectForKey:@"CFBundleVersion"];
-	NSString* licence_string = @"This program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n\
+	NSString* licence_string = @"This program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\
 	(at your option) any later version.\n\
-	\n\
 	This program is distributed in the hope that it will be useful,\
 	but WITHOUT ANY WARRANTY; without even the implied warranty of\
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\
-	GNU General Public License for more details.\n\
+	GNU General Public License for more details.\
 	You should have received a copy of the GNU General Public License\
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.";
+	along with this program. If not, see <http://www.gnu.org/licenses/>.";
 	return [BRAlertController alertOfType:0 titled:@"About" 
                             primaryText:[NSString stringWithFormat:@"XBMCLauncher %@", version]
-													secondaryText:[NSString stringWithFormat:@"Contributors: Scott Davilla / Stephan Diederich\nsee http://atv-xbmc-launcher.googlecode.com or http://www.xbmc.org for details\nCopyright (C) 2008 Team-XBMC\n%@",licence_string
+													secondaryText:[NSString stringWithFormat:@"Contributors: Scott Davilla / Stephan Diederich\nsee http://atv-xbmc-launcher.googlecode.com or www.xbmc.org for details\nCopyright (C) 2008 Team-XBMC\n%@",licence_string
 																				 ]];
 	
 }
