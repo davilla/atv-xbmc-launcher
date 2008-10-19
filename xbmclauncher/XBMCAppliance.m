27639af90569cf955a4fc7ec03168dafc636ecb7
stephan.diederich <stephan.diederich@af6a6984-82a5-11dd-ad27-0f66e57d2035>
2008-10-19 14:36:54 +0000

    removed some additional whitespaces

--- xbmclauncher/XBMCAppliance.m
+++ xbmclauncher/XBMCAppliance.m
@@ -106,13 +106,13 @@ typedef enum {
   else
     version = [info_dic objectForKey:@"CFBundleVersion"];
 	NSString* licence_string = @"This program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\
-	(at your option) any later version.\
-	This program is distributed in the hope that it will be useful,\
-	but WITHOUT ANY WARRANTY; without even the implied warranty of\
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\
-	GNU General Public License for more details.\
-  You should have received a copy of the GNU General Public License\
-  along with this program. If not, see <http://www.gnu.org/licenses/>.";
+(at your option) any later version.\
+This program is distributed in the hope that it will be useful,\
+but WITHOUT ANY WARRANTY; without even the implied warranty of\
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\
+GNU General Public License for more details.\
+You should have received a copy of the GNU General Public License\
+along with this program. If not, see <http://www.gnu.org/licenses/>.";
 	return [BRAlertController alertOfType:0 titled:@"About" 
                             primaryText:[NSString stringWithFormat:@"XBMCLauncher %@", version]
 													secondaryText:[NSString stringWithFormat:@"Contributors: Scott Davilla / Stephan Diederich Copyright 2008 Team-XBMC\nsee http://atv-xbmc-launcher.googlecode.com or www.xbmc.org for details\n%@",licence_string
