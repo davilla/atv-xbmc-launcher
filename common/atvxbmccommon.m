3c41b476ec51320204c6e04b9a9592ad2183fccc
stephan.diederich <stephan.diederich@af6a6984-82a5-11dd-ad27-0f66e57d2035>
2008-11-22 14:28:56 +0000

    tagged 0.7

--- common/atvxbmccommon.m
+++
@@ -1,6 +0,0 @@
-#import "atvxbmccommon.h"
-
-NSString* MULTIFINDER_START_APPLICATION_NOTIFICATION = @"org.multifinder.StartApplication";
-NSString* kApplicationPath = @"ApplicationPath";
-NSString* kApplicationNeedsIR = @"ApplicationNeedsIR";
-NSString* kApplicationWantsUniversalIRMode = @"ApplicationWantUniversalIRMode";
