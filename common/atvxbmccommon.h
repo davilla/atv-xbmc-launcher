6fb86b4b7ed36cc21f6f927b5d8019ad50eaa6bc
stephan.diederich <stephan.diederich@af6a6984-82a5-11dd-ad27-0f66e57d2035>
2008-11-22 13:57:49 +0000

    strings and stuff used in several projects

---
+++ common/atvxbmccommon.h
@@ -0,0 +1,10 @@
+#ifndef ATVXBMCCOMMON_H
+#define ATVXBMCCOMMON_H
+#import <Foundation/Foundation.h>
+
+extern NSString* MULTIFINDER_START_APPLICATION_NOTIFICATION;
+extern NSString* kApplicationPath;
+extern NSString* kApplicationNeedsIR;
+extern NSString* kApplicationWantsUniversalIRMode;
+
+#endif
