bbfd5bbb5a6767537b5f0e518e983c672b0fdcfb
stephan.diederich <stephan.diederich@af6a6984-82a5-11dd-ad27-0f66e57d2035>
2008-12-15 14:08:16 +0000

    added new update repository for MultiFinder and updated download urls

--- MultiFinder/Preferences.m
+++
@@ -1,7 +0,0 @@
-#import "Preferences.h"
-#import "XBMCDebugHelpers.h"
-
-NSString* const kMFDefaultApp = @"DefaultApplication";
-NSString* const kMFDefaultAppIRMode = @"DefaultApplicationIRMode";
-NSString* const kMFAppLaunchMaxRetryCount = @"AppLaunchMaxRetryCount";
-
