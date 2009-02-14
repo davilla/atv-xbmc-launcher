ef42e63c6a10c99666d9fbb565c5ad45c46ec874
stephan.diederich <stephan.diederich@af6a6984-82a5-11dd-ad27-0f66e57d2035>
2009-02-14 17:43:44 +0000

    added repository for Launcher 3.0 aka unified launcher

--- common/atvxbmccommon.m
+++
@@ -1,9 +0,0 @@
-#import "atvxbmccommon.h"
-
-NSString* MULTIFINDER_START_APPLICATION_NOTIFICATION = @"org.multifinder.StartApplication";
-NSString* MULTIFINDER_CHANGE_DEFAULT_APPLICATION_NOTIFICATION = @"org.multifinder.ChangeDefaultApp";
-NSString* kApplicationPath = @"ApplicationPath";
-NSString* kApplicationArguments = @"ApplicationArguments";
-NSString* kApplicationNeedsIR = @"ApplicationNeedsIR";
-NSString* kApplicationWantsUniversalIRMode = @"ApplicationWantUniversalIRMode";
-
---
+++ scripts/launcher-update-3.0.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+INSTALLER=$1
+PW="frontrow"
+REMOUNT=0
+
+echo "Installing file $INSTALLER"
+
+# check that file exists
+if [ -e $INSTALLER ]; then
+
+ # Check if / is mounted read only
+ if mount | grep ' on / '  | grep -q 'read-only'; then
+  REMOUNT=1
+  echo $PW | sudo -S /sbin/mount -uw /
+ fi
+
+ #delete boxeelauncher 0.1 if present
+ echo $PW | sudo -S rm -rf /System/Library/CoreServices/Finder.app/Contents/PlugIns/BoxeeLauncher.frappliance
+ 
+ #reset default finder
+echo $PW | sudo -S defaults delete /Library/Preferences/com.apple.loginwindow Finder
+ 
+ #install MultiFinder/Launcher combo
+ echo $PW | sudo -S chmod +x $INSTALLER
+ echo $PW | sudo -S $INSTALLER -- install /
+  
+ #remove the download
+ rm $INSTALLER 
+
+ #sync to disk, just in case...
+ /bin/sync
+ 
+ # remount root as we found it
+ if [ "$REMOUNT" = "1" ]; then
+  echo $PW | sudo -S /sbin/mount -ur /
+ fi
+
+ # restart loginwindow
+ kill `ps awwx | grep [l]oginwindow | awk '{print $1}'`
+ kill `ps awwx | grep [F]inder | grep -v launcher-update | awk '{print $1}'`
+  
+ exit 0
+fi
+echo "Failed to find installer $INSTALLER"
+exit -1
+
