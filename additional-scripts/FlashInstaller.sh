633921a5ba5cc81ce7223e0daed173ad3003f646
Jim.wiley.arc <Jim.wiley.arc@af6a6984-82a5-11dd-ad27-0f66e57d2035>
2010-06-17 19:58:02 +0000

    must create dir Internet Plug-Ins if it doesn't exist

--- additional-scripts/FlashInstaller.sh
+++ additional-scripts/FlashInstaller.sh
@@ -4,7 +4,7 @@
 
 DISKIMAGE=$1
 PW="frontrow"
-plugdir=/Users/frontrow/Library/Internet\ Plug-Ins/
+plugdir=/Library/Internet\ Plug-Ins/
 
 echo "Installing from diskimage $DISKIMAGE"
 
@@ -20,6 +20,11 @@ if [ -e $DISKIMAGE ]; then
     echo $PW | sudo -S /sbin/mount -uw /
   fi
 
+### Create Plugin DIr if it does not exist ###
+  echo $PW | sudo -S mkdir -p "$plugdir"
+
+
+
   ### UNINSTALL PREVIOUS FLASH VERSIONS ###
   if [[ -d "$plugdir"Flash\ Player.plugin  ]]; then
 	echo $PW | sudo -S rm -rf "$plugdir"Flash\ Player.plugin
@@ -34,28 +39,18 @@ if [ -e $DISKIMAGE ]; then
   	echo $PW | sudo -S rm -f /Library/Internet\ Plug-Ins/flashplayer.xpt
   fi
   
-  
-  
+    
   ### INSTALL FLASH PLUGIN ###  
   echo $PW | sudo -S hdiutil attach $DISKIMAGE 
-  echo $PW | sudo -S pax -r -z -f /Volumes/Install\ Flash\ Player\ 10\ UB/Adobe\ Flash\ Player.pkg/Contents/Archive.pax.gz
-  echo $PW | sudo -S rm -Rd Flash\ Player.plugin/Contents/Resources/{c,d,f,i,j,k,n,p,r,s,t,z,e}*.lproj
-  echo $PW | sudo -S mv Flash\ Player.plugin/ "$plugdir"
-  echo $PW | sudo -S mv flashplayer.xpt "$plugdir"
-  echo $PW | sudo -S chown -R frontrow:frontrow "$plugdir"
-  
- 
-  echo $PW | sudo -S ln -s "$plugdir"Flash\ Player.plugin /Library/Internet\ Plug-Ins/
-  echo $PW | sudo -S ln -s "$plugdir"flashplayer.xpt /Library/Internet\ Plug-Ins/  
-   
-
-  echo $PW | sudo -S hdiutil detach /Volumes/Install\ Flash\ Player\ 10\ UB/
+  echo "DYLD_FRAMEWORK_PATH='/System/Library/Frameworks/OSXFrames' installer -pkg /Volumes/Flash\ Player/Install\ Adobe\ Flash\ Player.app/Contents/Resources/Adobe\ Flash\ Player.pkg -target /" | sudo -s
+  echo "exit"
+  echo $PW | sudo -S hdiutil detach /Volumes/Flash\ Player
 
   #sync to disk, just in case...
   /bin/sync
   
   # clean up
-  echo $PW | sudo -S rm /Users/frontrow/Movies/Install\ Flash\ Player\ 10\ UB
+  echo $PW | sudo -S rm /Users/frontrow/Movies/Flash\ Player
  
 
   # restore OSBoot read/write settings
